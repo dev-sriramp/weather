eb2c3e73d06d57263a3f4d07352cf10033b9b70b
uniqueredhat <uniqueredhat@gmail.com>
2021-06-28 23:46:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {apiKey} from './Components/Constants'
+console.log(apiKey);
+var api_key = '1cc89089ee4169641fac7a0703ae9e9a';
+var complete_api_link = 'https://api.openweathermap.org/data/2.5/weather?q=coimbatore&appid='+''+apiKey;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
     </div>
   );
 }
